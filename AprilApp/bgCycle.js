8e327c85f898463da421812db4b16788782d3d08
In-Young Jo <ij8@duke.edu>
2014-06-28 22:55:41 -0400

    nm

--- AprilApp/bgCycle.js
+++ AprilApp/bgCycle.js
@@ -47,7 +47,7 @@ function bgCycle(){
     		}
     		element.style.opacity = op;
     		element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-    		op += .05;
+    		op += .001;
     	}, 50);
 	}
 
