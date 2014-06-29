59e6479bdf74731e66dd9726289eb5fbe20f2706
In-Young Jo <ij8@duke.edu>
2014-06-28 22:18:11 -0400

    added cycling code

--- AprilApp/bgCycle.js
+++ AprilApp/bgCycle.js
@@ -1,5 +1,29 @@
 function bgCycle(){
-	e=document.getElementById("container");
-	
-	document.body.style.backgroundImage="url('img_tree.png')";
+
+	function displayImage(image) {
+		// document.getElementById("img").src = image;
+		document.getElementById("container").style.backgroundImage = url(image);
+	}
+
+	function displayNextImage() {
+		x = (x == images.length - 1) ? 0 : x + 1;
+		displayImage(images[x]);           
+	}
+
+	function displayPreviousImage() {
+		x = (x <= 0) ? images.length - 1 : x - 1;
+		displayImage(images[x]);           
+	}
+
+	function startTimer() {
+		setInterval(displayNextImage, 3000);
+	}
+
+	var images = [], x = -1;
+	images[0] = "img/NYBG.jpg";
+	images[1] = "img/RoadTripBG.jpg";
+	images[2] = "img/NYBG.jpg";
+
+	startTimer();
 }
+window.onload=bgCyle;
