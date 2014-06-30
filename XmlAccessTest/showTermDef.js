c6a4d01c0cc7fbb05ee1850b13e76612b8d93086
In-Young Jo <ij8@duke.edu>
2014-06-30 16:25:13 -0400

    Update showTermDef.js

--- XmlAccessTest/showTermDef.js
+++ XmlAccessTest/showTermDef.js
@@ -13,8 +13,9 @@ function showTermDef(searchTerm)	{
 	f=document.getElementById("definition");
 	var x = xmlDoc.getElementsByTagName("TermDef");
 	for (i=0;i<x.length;i++)	{
-		if(x[i].getElementsByTagName("Term")[0].childNodes[0].nodeValue.toLowerCase() == 
- +		document.getElementById("query").toLowerCase)	{
+		//if(x[i].getElementsByTagName("Term")[0].childNodes[0].nodeValue.toLowerCase() == document.getElementById("query").toLowerCase)	{
+		if(x[i].getElementsByTagName("Term")[0].childNodes[0].nodeValue.toLowerCase() == 'bankruptcy')	{
+
 			e.innerHTML = x[i].getElementsByTagName("Term")[0].childNodes[0].nodeValue;
 			f.innerHTML = x[i].getElementsByTagName("Definition")[0].childNodes[0].nodeValue;
 			break;
